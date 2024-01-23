fdde2ce5dd7a189719774eb07c8deb24713c09f0
Manoj <manoj09633@gmail.com>
2024-01-23 20:38:40 +0530

    Basic triangle pattern

--- src/com/java/patterns/BasicTriangle.java
+++ src/com/java/patterns/BasicTriangle.java
@@ -4,8 +4,11 @@ public class BasicTriangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int line=1; line<=4; line++) {
-			
+		for (int line = 1; line <= 4; line++) {
+			for (int star = 1; star <= line; star++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 	}
 
