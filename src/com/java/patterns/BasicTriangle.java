84f171d2595ea84b633b18190a0233e5ba2bcd82
Manoj <manoj09633@gmail.com>
2024-01-23 20:35:14 +0530

    Inital commit

---
+++ src/com/java/patterns/BasicTriangle.java
@@ -0,0 +1,12 @@
+package com.java.patterns;
+
+public class BasicTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int line=1; line<=4; line++) {
+			
+		}
+	}
+
+}
